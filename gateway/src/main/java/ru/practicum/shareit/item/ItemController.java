53d10ff832c6e4228cae9ca521a2f50c464c41fa
UvarovaJulia <uvarovater@yandex.ru>
2024-03-16 00:50:12 +0300

    исправлена проверка /search на пустой запрос

--- gateway/src/main/java/ru/practicum/shareit/item/ItemController.java
+++ gateway/src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -33,34 +33,34 @@ public class ItemController {
 
     @PatchMapping("/{itemId}")
     public ResponseEntity<Object> updateItem(@RequestHeader(Constant.USER_ID) long userId,
-                                   @PathVariable long itemId,
-                                   @Validated(Marker.OnUpdate.class) @RequestBody ItemDtoShort item) {
+                                             @PathVariable long itemId,
+                                             @Validated(Marker.OnUpdate.class) @RequestBody ItemDtoShort item) {
         log.info("Получен запрос от пользователя с id {} на обновление вещи", userId);
         return itemClient.updateItem(userId, itemId, item);
     }
 
     @GetMapping("/{itemId}")
     public ResponseEntity<Object> getItem(@RequestHeader(Constant.USER_ID) long userId,
-                           @PathVariable long itemId) {
+                                          @PathVariable long itemId) {
         log.info("Получен запрос на получение вещи с id {} от пользователя {}", itemId, userId);
         return itemClient.getItem(itemId, userId);
     }
 
     @GetMapping
     public ResponseEntity<Object> getItemsByUser(@RequestHeader(Constant.USER_ID) long userId,
-                                        @RequestParam(name = "from", defaultValue = "0") @PositiveOrZero Integer from,
-                                        @RequestParam(name = "size", defaultValue = "5") @Positive Integer size) {
+                                                 @RequestParam(name = "from", defaultValue = "0") @PositiveOrZero Integer from,
+                                                 @RequestParam(name = "size", defaultValue = "5") @Positive Integer size) {
         log.info("Получен запрос от пользователя с id {} на получение списка его вещей", userId);
         return itemClient.getItemsByUser(userId, from, size);
     }
 
     @GetMapping("/search")
     public ResponseEntity<Object> searchItems(@RequestParam String text,
-                                          @RequestParam(name = "from", defaultValue = "0") @PositiveOrZero Integer from,
-                                          @RequestParam(name = "size", defaultValue = "5") @Positive Integer size) {
+                                              @RequestParam(name = "from", defaultValue = "0") @PositiveOrZero Integer from,
+                                              @RequestParam(name = "size", defaultValue = "5") @Positive Integer size) {
         if (text.isBlank()) {
             log.info("Получен список из 0 вещей по запросу '{}'", text);
-            return (ResponseEntity<Object>) Collections.emptyList();
+            return ResponseEntity.ok(Collections.emptyList());
         }
         log.info("Получен запрос на поиск {} среди вещей", text);
         return itemClient.searchItems(text, from, size);
@@ -68,8 +68,8 @@ public class ItemController {
 
     @PostMapping("/{itemId}/comment")
     public ResponseEntity<Object> createComment(@PathVariable long itemId,
-                                            @RequestHeader(Constant.USER_ID) long userId,
-                                            @Valid @RequestBody CommentDtoRequest comment) {
+                                                @RequestHeader(Constant.USER_ID) long userId,
+                                                @Valid @RequestBody CommentDtoRequest comment) {
         log.info("Получен запрос на оставление комментария на вещь {}", itemId);
         return itemClient.createComment(itemId, userId, comment);
     }
